926e15b8fdca81d5b7fdea9848dc30e5b1e5afa3
LiQiangZhang <changlq@hotmail.com>
2012-04-05 19:04:42 -0400

    Finished layout and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+module ApplicationHelper
+
+  def title
+    base_title = "ROR sample app"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+end
 end
